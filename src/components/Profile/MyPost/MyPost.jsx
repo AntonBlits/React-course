6a95cae8fecb544e91a26936e26edd54f2a9a8ed
Anton <anton-zyablitsev@mail.ru>
2022-03-17 07:35:59 +0300

    Finished lesson 16 - props

--- src/components/Profile/MyPost/MyPost.jsx
+++ src/components/Profile/MyPost/MyPost.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Post from './Post/Post';
 
 const MyPost = () => {
+   debugger;
    return (
       <div>my post
          <div>new post</div>
          <div>
-            <Post />
-            <Post />
+            <Post message='Hello, my name is Antony' amount='17' />
+            <Post message='Hello React developer!' amount='13' />
          </div>
       </div>
    );
