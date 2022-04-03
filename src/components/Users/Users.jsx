db46db6cb7c20ca3eea2c364d9473482ce5545a2
Anton <anton-zyablitsev@mail.ru>
2022-04-03 15:41:56 +0300

    lesson 49

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,19 +3,32 @@ import s from "./Users.module.css";
 
 const Users = (props) => {
    return <div>
-      {props.users.map(u => <div key={u.id} className={s.user}>
-         <div className={s.avatar}>
-            <div>
-               <img src={u.avatar} />
+      <h2 className={s.h2}>Users</h2>
+      <div className={s.users}>
+         {props.users.map(u => <div key={u.id} className={s.user}>
+            <div className={s.avatar}>
+               <div>
+                  <img src={u.avatar} />
+               </div>
+               <div>
+                  {u.followed
+                     ? <button className={s.btm} onClick={() => props.unfollow(u.id)}>Unfollow</button>
+                     : <button className={s.btm} onClick={() => props.follow(u.id)}>Follow</button>}
+               </div>
             </div>
-            <div>
-               {u.followed
-                  ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
-                  : <button onClick={() => props.follow(u.id)}>Follow</button>}
+            <div className={s.info}>
+               <div className={s.fullname}>
+                  <div>{u.fullname}</div>
+                  <div className={s.status}>{u.status}</div>
+               </div>
+               <div className={s.location}>
+                  <div>{u.location.city}</div>
+                  <div>{u.location.country}</div>
+
+               </div>
             </div>
-         </div>
-         <div className={s.info}></div>
-      </div>)}
+         </div>)}
+      </div>
    </div>
 }
 
